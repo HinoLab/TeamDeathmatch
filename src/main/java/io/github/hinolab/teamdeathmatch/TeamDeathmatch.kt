c1bf0d761850615eb85592c88ecb997a589f2de2
M1n1don <contact@m1n1don.dev>
2023-11-26 23:17:25 +0900

    [Change] 言語を変更 Java -> Kotlin

---
+++ src/main/java/io/github/hinolab/teamdeathmatch/TeamDeathmatch.kt
@@ -0,0 +1,13 @@
+package io.github.hinolab.teamdeathmatch
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TeamDeathmatch : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
